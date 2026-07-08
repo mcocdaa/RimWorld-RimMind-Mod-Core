acce8152c8c37089386eb0cb5670f43d44f5e515
mcocdaa <2021137961@qq.com>
2026-07-08 17:31:56 +0800

    chore: archive 14 legacy backup files from Source/backup to Refs/backup

--- Source/backup/IScopedAgent.cs
+++
@@ -1,9 +0,0 @@
-namespace RimMind.Application.Common.Interfaces.Agent
-{
-    public interface IScopedAgent : IAgentControl
-    {
-        string ScopeId { get; }
-        string ScopeType { get; }
-        int? MapId { get; }
-    }
-}
