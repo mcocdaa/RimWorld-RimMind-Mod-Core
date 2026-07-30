fd3206d499e5e04054b92a4d6535546375601e4b
mcocdaa <2021137961@qq.com>
2026-07-30 19:36:26 +0800

    test: remove approved retired tests

--- IntegrationTests/VerseStubs.cs
+++
@@ -1,9 +0,0 @@
-using RimMind.Application.Common.Interfaces.Abstractions;
-
-namespace RimMind.IntegrationTests.Stubs
-{
-    internal sealed class TestPathProvider : IPathProvider
-    {
-        public string SaveDataFolderPath => System.IO.Path.GetTempPath();
-    }
-}
